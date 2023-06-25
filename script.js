3073250222c691818cf2aab7ba03ec2808ff4a43
Musa Kamara <kamaramusa56@gmail.com>
2023-06-26 00:39:32 +0300

    first character string added

--- script.js
+++ script.js
@@ -12,5 +12,23 @@ function removeZero(numbers){
 let result=removeZero(nums);
 console.log(result)
 
-
 // question 2
+function firstUniqChar(s){
+    let obj={}
+    for(let char of s){
+        if(!obj[char]){
+            obj[char]=1
+        } else {
+            obj[char]++
+        }
+    }
+    for (let key in obj){
+        if(obj[key]===1){
+            return s.indexOf(key)
+        }
+    }
+
+    return -1
+}
+
+console.log(firstUniqChar('youtube'));
